822078d45c3d43bbf10db380c20956a0568294bc
unknown <ngominhkhoa2006@gmail.com>
2025-08-30 12:34:30 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,html}",
+    "./src/commands/*.html",
+    "./src/taskpane/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
